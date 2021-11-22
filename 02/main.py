d31520460c3922de37f3113777c471ea71336add
Tommy Duchesne <tommy.duchesne2@gmail.com>
2021-11-21 22:54:53 -0500

    02: add solution of second part

--- 02/main.py
+++ 02/main.py
@@ -8,7 +8,7 @@ this_dir = os.path.dirname(__file__)
 with open(this_dir + "/input", mode='r') as file:
     input_ = [line.strip() for line in file.readlines() if line]
 
-good_password_count = 0
+good_password_count1 = 0
 for line in input_:
     password_policy_expression = re.compile(r'^(\d+)-(\d+) (\w): (\w+)')
     match = password_policy_expression.match(line)
@@ -23,6 +23,27 @@ for line in input_:
     password = groups[3]
 
     if lower <= password.count(character) <= upper:
-        good_password_count += 1
+        good_password_count1 += 1
 
-print(good_password_count)
+good_password_count2 = 0
+for line in input_:
+    password_policy_expression = re.compile(r'^(\d+)-(\d+) (\w): (\w+)')
+    match = password_policy_expression.match(line)
+
+    if not match:
+        break
+
+    groups = match.groups()
+    lower = int(groups[0]) - 1
+    upper = int(groups[1]) - 1
+    character = groups[2]
+    password = groups[3]
+
+    if (password[lower] == character) ^ (password[upper] == character):
+        good_password_count2 += 1
+
+print("First part:")
+print(good_password_count1)
+print("\nSecond part:")
+print(good_password_count2)
+
