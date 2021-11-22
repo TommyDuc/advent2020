1997731c18a5a5a7f1aa1e25f120f83daf46a04c
Tommy Duchesne <tommy.duchesne2@gmail.com>
2021-11-21 20:13:39 -0500

    01: add second part solution

--- 01/main.py
+++ 01/main.py
@@ -9,7 +9,19 @@ with open(this_dir + "/input", mode='r') as file:
 
 # print("\n".join([str(i) for i in input_]))
 
-found_candidates = [i for i in input_ if 2020 - i in input_]
-print(found_candidates)
-print(found_candidates[0]*found_candidates[1])
+answer1 = [i for i in input_ if 2020 - i in input_]
 
+answer2 = []
+for num1 in input_:
+    first_difference = 2020 - num1
+    new_candidates = [i for i in input_ if first_difference - i in input_]
+    if len(new_candidates) == 2:
+        answer2 = (num1, new_candidates[0], new_candidates[1])
+        break
+
+print("First part:")
+print(answer1)
+print(answer1[0] * answer1[1])
+print("\nSecond part:")
+print(answer2)
+print(answer2[0] * answer2[1] * answer2[2])
